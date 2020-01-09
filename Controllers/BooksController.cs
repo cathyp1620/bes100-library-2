cfef381642081affd6b2b173566cf6fccf9b2589
cathyp1620 <59615043+cathyp1620@users.noreply.github.com>
2020-01-09 08:48:42 -0500

    Initial commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿
+using LibraryApi.Domain;
+using LibraryApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class BooksController : Controller
+    {
+        LibraryDataContext Context;
+
+        public BooksController(LibraryDataContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet("/books")]
+        public async Task<IActionResult> GetAllBooks([FromQuery] string genre ="all")
+        {
+            var response = new GetBooksResponseCollection();
+            var allBooks = Context.Books.Select(b => new BookSummaryItem
+            {
+                Id = b.Id,
+                Title = b.Title,
+                Author = b.Author,
+                Genre = b.Genre
+            });
+            if(genre != "all")
+            {
+                allBooks = allBooks.Where(b => b.Genre == genre);
+            }
+            response.Books = await allBooks.ToListAsync();
+            response.GenreFilter = genre;
+            
+            return Ok(response);
+        }
+    }
+}
